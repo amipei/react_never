02ca19a7cb1633547fdae239d2ff7ba7c28788ba
amipei <amipei1020@gmail.com>
2019-08-15 16:52:10 +0800

    Updates

---
+++ precache-manifest.962f7062aac2a7e8f1805a755248c70c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b360a702318901748ea4344171237b",
+    "url": "/react_never/index.html"
+  },
+  {
+    "revision": "e325f34b5ed24502c9b7",
+    "url": "/react_never/static/css/main.603a3df9.chunk.css"
+  },
+  {
+    "revision": "c6350e6ba9575de19b68",
+    "url": "/react_never/static/js/2.94adb150.chunk.js"
+  },
+  {
+    "revision": "e325f34b5ed24502c9b7",
+    "url": "/react_never/static/js/main.7a2ef4c2.chunk.js"
+  },
+  {
+    "revision": "b29d77853ffbc6b30cec",
+    "url": "/react_never/static/js/runtime~main.43a0e62c.js"
+  }
+]);
